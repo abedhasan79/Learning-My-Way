35d20679e3f37c3ed3fc5d8d0868e6d17b77994c
unknown <alicia9790972@gmail.com>
2022-09-28 14:11:25 -0400

    api fiddling

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,25 +3,18 @@ var searchBar = $("#bar");
 var searchBtn = $("#searchButton");
 
 // api
-//flick
-var flickApiLink =
-  "https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg";
-var flickApiKey = "bf5af6f6035bbed9eaae9ff37805fd5e";
-var flickApiSecret = "8fa38f3db4dff9f6";
-
 //splash
-var splashApiLink = "https://api.unsplash.com/photos/?client_id=";
+var splashApiLink = "https://api.unsplash.com/photos/:id?q=";
 var splashApiKey = "vhzjXLz3kbF0cpvBjxXkR3zMRbAB2eYwP7qI5kxHndQ";
 var splashApiSecret = "aCQVXryiUwzsD6CuMJ-RjMNCmKNXB2zv6dmkLiW0-sc";
 
 //function to start once button clicked
 function searchLandmark() {
   var landmarkName = searchBar.val().trim();
-  fetch(splashApiLink + landmarkName + splashApiSecret);
-  console.log(landmarkName);
+  fetch(splashApiLink + landmarkName + "client_id=" + splashApiKey);
 }
 
 //search bar click function
 searchBtn.on("click", searchLandmark);
 
-//flic.kr/p/[short-photo-id] use url shortner
+//https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
