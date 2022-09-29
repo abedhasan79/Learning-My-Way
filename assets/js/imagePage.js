4a37a1bf38be106a9881d951bf9a713dd71ccc49
Abed Hasan <abedhasan79@gmail.com>
2022-09-29 18:49:57 -0400

    got fata from youtube and unsplash show up

--- assets/js/imagePage.js
+++ assets/js/imagePage.js
@@ -4,25 +4,29 @@
 /////////////////////////////////////////////////////
 // Youtube getting data and videos
 
-let youtubeAPI = 'AIzaSyC0s_3--Ea3TtqQ_yU0BJw8NSYdHMwAgD0';
-let search ="Jackson Park windsor"
+// let youtubeAPI = 'AIzaSyC0s_3--Ea3TtqQ_yU0BJw8NSYdHMwAgD0';
+// let search ="Jackson Park windsor"
 
-let urlReqs = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&maxResults=10&q='+search+'&key='+youtubeAPI;
-var youtubeUrl = `https://www.youtube.com/watch?v=`;
+// let urlReqs = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelType=any&maxResults=10&q='+search+'&key='+youtubeAPI;
+// var youtubeUrl = `https://www.youtube.com/embed/`;
 
 
 
-fetch(urlReqs)
-    .then(function (response) {
-        console.log(response.status);
-        return response.json();
-    })
-    .then(function (data,) {
-        console.log(data);
-        console.log(data.items[0].snippet.title);
-        $('.description').attr('src',data.items[0].snippet.thumbnails.default.url);
-        $('a').attr('href',(youtubeUrl+data.items[0].id.videoId));
-    });
+
+// fetch(urlReqs)
+//     .then(function (response) {
+//         console.log(response.status);
+//         return response.json();
+//     })
+//     .then(function (data,) {
+//         console.log(data);
+//         console.log(data.errors.message);
+//         $('.youTubeVideos1').attr('src',youtubeUrl+data.items[0].id.videoId);
+//         $('.youTubeVideos2').attr('src',youtubeUrl+data.items[1].id.videoId);
+//         $('.youTubeVideos3').attr('src',youtubeUrl+data.items[2].id.videoId);
+//         $('.youTubeVideos4').attr('src',youtubeUrl+data.items[3].id.videoId);
+        
+//     });
 
 ///////////////////////////////////////////////////////
 
@@ -60,7 +64,7 @@ fetch(urlReqs)
 
 
 let splashApiKey = "vhzjXLz3kbF0cpvBjxXkR3zMRbAB2eYwP7qI5kxHndQ";
-let imageUrl = 'https://api.unsplash.com/search/photos?client_id='+splashApiKey+'&query='+'dogs'+'&page=1';
+let imageUrl = 'https://api.unsplash.com/search/photos?client_id='+splashApiKey+'&query='+'CN tower'+'&page=2';
 
 fetch(imageUrl)
     .then(function (response) {
@@ -69,20 +73,27 @@ fetch(imageUrl)
     })
     .then(function (data,) {
         console.log(data);
-        $('.image').attr('src',data.results[0].urls.full);
+        $('.img1').attr('src',data.results[0].urls.full);
+        $('.img2').attr('src',data.results[1].urls.full);
+        $('.img3').attr('src',data.results[2].urls.full);
+        $('.img4').attr('src',data.results[3].urls.full);
+        $('.img5').attr('src',data.results[4].urls.full);
         
     });
 
 
-let searchWiki = 'Jackson park windsor'
-let urlReqs2 = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search="+searchWiki+"&limit=5&namespace=0&format=json";
+// let searchWiki = 'Jackson park windsor'
+// let urlReqs2 = "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search="+searchWiki+"&limit=5&namespace=0&format=json";
 
-fetch(urlReqs2)
-    .then(function (response) {
-        console.log(response.status);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        $('.article').attr('href',data[3][0] );
-    });
+// fetch(urlReqs2)
+//     .then(function (response) {
+//         console.log(response.status);
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         console.log(data);
+//         $('.article').attr('href',data[3][0] );
+//     });
+
+
+
